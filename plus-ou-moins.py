1e5d8c4e168edf4c03cd798109bc3d1928a3dde2
Kikii95 <147034133+Kikii95@users.noreply.github.com>
2023-11-20 14:30:21 +0100

    v2 plus ou moins

--- plus-ou-moins.py
+++ plus-ou-moins.py
@@ -1,36 +1,57 @@
 from random import *
+
+def AskInt() -> int:
+    try:
+        () = int()
+    except:
+        print("Veillez a bien entrer un chiffre")
+        continue   
+    
+
 while True :
     print("---------------------<3---------------------")
-    essais_max= int(input("Choisissez le nombre de tentatives possibles :"))
-    essais = 1   
-    nombre_joueur = 0   
-    nombre_min_ordi = int(input("Choisissez une borne minimale :"))
-    nombre_max_ordi = int(input("Choisissez une borne maximale :"))
-    nombre_ordi = randint(nombre_min_ordi,nombre_max_ordi)
+    while True:
+        attempts_max= input("Choisissez le nombre de tentatives possibles :")
+        AskInt(attempts_max)
+    attempts = 1
+    score = 0   
+    min_nb = (input("Choisissez une borne minimale :"))
+    try:
+        min_nb = int(min_nb) and min_nb < 100
+    except:
+        print("La borne minimale doit etre un nombre plus petite que celui de la borne maximale !")
+        continue   
+    max_nb = (input("Choisissez une borne maximale :"))
+    try:
+        max_nb = int(max_nb) and min_nb < max_nb and max_nb <=100
+    except:
+        print("La borne maximale doit etre un nombre plus grand que celui de la borne minimale !")
+        continue   
+    nb = randint(min_nb,max_nb)
 
     print("---------------------<3---------------------")
-    print("Les bornes sont ",nombre_min_ordi," et ",nombre_max_ordi, ".")
-    print("Trouvez le nombre en moins de ",essais_max," tentatives si vous voulez gagner.")
+    print("Les bornes sont ",min_nb," et ",max_nb, ".")
+    print("Trouvez le nombre en moins de ",attempts_max," tentatives si vous voulez gagner.")
 
     print("---------------------<3---------------------")
-    while nombre_joueur != nombre_ordi and essais <= essais_max:
-        essais_restants = essais_max - essais+1
+    while score != nb and attempts <= attempts_max:
+        remaining_attempts = attempts_max - attempts+1
         print("---------------------<3---------------------")
-        print("Il vous reste",essais_restants,"tentatives.")
-        nombre_joueur = int(input("Choisissez un nombre :"))
-        if nombre_joueur < nombre_ordi:
+        print("Il vous reste",remaining_attempts,"tentatives.")
+        score = int(input("Choisissez un nombre :"))
+        if score < nb:
             print("Plus !")
-        elif nombre_joueur > nombre_ordi:
+        elif score > nb:
             print("Moins !")
         else:
             print("---------------------<3---------------------")
-            print("Bravo ! Vous avez trouvé le nombre ","en",essais,"essai(s)")
-        essais += 1
+            print("Bravo ! Vous avez trouvé le nombre ","en",attempts,"essai(s)")
+        attempts += 1
 
-    if essais>essais_max and nombre_joueur != nombre_ordi :
+    if attempts>attempts_max and score != nb :
         print("---------------------<3---------------------")
         print("Vous n'avez plus aucune tentative.")
-        print("Dommage ! Le bon nombre était : ",nombre_ordi,".")
+        print("Dommage ! Le bon nombre était : ",nb,".")
 
         
     reponse = input("Souhaitez-vous relancer une partie (Y/N) ?")
