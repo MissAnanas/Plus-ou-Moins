b696967a14de59228aecc6d50eec9b67fe5939b8
Kikii95 <147034133+Kikii95@users.noreply.github.com>
2023-11-21 14:48:14 +0100

    Final version ++++++

--- plus-ou-moins.py
+++ plus-ou-moins.py
@@ -1,6 +1,6 @@
-from random import *
+from random import randint
 
-def AskInt(message: str, min_val: int, max_val: int) -> int:
+def ask_int(message: str, min_val: int, max_val: int) -> int:
     while True:
         try:
             user_input = int(input(message))
@@ -18,80 +18,83 @@ def relaunch_game():
         return True, use_same_params
     return False, False
 
-print("---------------------<3---------------------")
-print("Bienvenue sur ce jeu de plus ou moins !")
-reponse = input("Souhaitez-vous consulter les règles (Y/N) ? ")
-if reponse.upper() == "Y":
-    print("---------------------<3---------------------")
-    print("Règles :")
-    print("- Le but du jeu est de deviner le chiffre choisi par l'ordinateur. - ")
-    print("------> - Définissez le nombre de tentatives que vous aurez,")
-    print("------> - Choisissez 2 bornes entre lesquelles un chiffre sera choisi aléatoirement,")
-    print("------> - Choisissez un nombre compris entre les bornes,")
-    print("------> - Plus ! alors votre nombre est plus petit que celui de l'ordinateur,")
-    print("------> - Moins ! alors votre nombre est plus grand que celui de l'ordinateur,")
-    print("------> - Attention à bien surveiller vos tentatives restantes !")
-    print("---------------------<3---------------------")
-        
-while True:
-    print("---------------------<3---------------------")
-    attempts_max = AskInt("Choisissez le nombre de tentatives possibles : ", 1, 100)
-    
+def play_one_game(min_val, max_val, attempts_max):
+    nb = randint(min_val, max_val)
     attempts = 1
     score = 0
 
-    min_nb = AskInt("Choisissez une borne minimale : ", 1, 100)
-    max_nb = AskInt("Choisissez une borne maximale : ", 1, 100)
+    while score != nb and attempts <= attempts_max:
+        remaining_attempts = attempts_max - attempts + 1
+        print("---------------------<3---------------------")
+        print(f"Le numéro se situe entre {min_val} et {max_val}.")
+        print("Il vous reste", remaining_attempts, "tentatives.")
+        score = ask_int("Choisissez un nombre : ", min_val, max_val)
 
-    if min_nb >= max_nb:
-        print("La borne minimale doit être strictement inférieure à la borne maximale. Veuillez recommencer.")
-        continue
+        if score < nb:
+            print("---------------------<3---------------------")
+            print("Plus que" ,score,"!")
+        elif score > nb:
+            print("---------------------<3---------------------")
+            print("Moins que" ,score,"!")
+        else:
+            print("---------------------<3---------------------")
+            print("Bravo ! Vous avez trouvé le nombre ", "en", attempts, "essai(s)")
+            break
+        attempts += 1
 
-    try:
-        assert min_nb < 100
-        assert min_nb < max_nb <= 100
-    except AssertionError:
-        print("Les bornes ne sont pas valides. Veuillez recommencer.")
-        continue
+    if attempts > attempts_max and score != nb:
+        print("---------------------<3---------------------")
+        print("Vous n'avez plus aucune tentative.")
+        print("Dommage ! Le bon nombre était : ", nb, ".")
+        print("---------------------<3---------------------")
+
+def launch_game():
+    print("---------------------<3---------------------")
+    print("Bienvenue sur ce jeu de plus ou moins !")
+    
+    reponse = input("Souhaitez-vous consulter les règles (Y/N) ? ")
+    if reponse.upper() == "Y":
+        print("---------------------<3---------------------")
+        print("Règles :")
+        print("- Le but du jeu est de deviner le chiffre choisi par l'ordinateur. - ")
+        print("------> - Définissez le nombre de tentatives que vous aurez,")
+        print("------> - Choisissez 2 bornes entre lesquelles un chiffre sera choisi aléatoirement,")
+        print("------> - Choisissez un nombre compris entre les bornes,")
+        print("------> - Plus ! alors votre nombre est plus petit que celui de l'ordinateur,")
+        print("------> - Moins ! alors votre nombre est plus grand que celui de l'ordinateur,")
+        print("------> - Attention à bien surveiller vos tentatives restantes !")
+        print("---------------------<3---------------------")
 
     while True:
-        nb = randint(min_nb, max_nb)
+        attempts_max = ask_int("Choisissez le nombre de tentatives possibles : ", 1, 100)
+        min_nb = ask_int("Choisissez une borne minimale : ", 1, 100)
+        max_nb = ask_int("Choisissez une borne maximale : ", 1, 100)
 
-        print("---------------------<3---------------------")
-        print("Les bornes sont ", min_nb, " et ", max_nb, ".")
-        print("Trouvez le nombre en moins de ", attempts_max, " tentatives si vous voulez gagner.")
+        if min_nb >= max_nb:
+            print("La borne minimale doit être strictement inférieure à la borne maximale. Veuillez recommencer.")
+            continue
 
-        while score != nb and attempts <= attempts_max:
-            remaining_attempts = attempts_max - attempts + 1
-            print("---------------------<3---------------------")
-            print("Il vous reste", remaining_attempts, "tentatives.")
-            score = AskInt("Choisissez un nombre : ", 1, 100)
+        try:
+            assert min_nb < 100
+            assert min_nb < max_nb <= 100
+        except AssertionError:
+            print("Les bornes ne sont pas valides. Veuillez recommencer.")
+            continue
 
-            if score < nb:
-                print("Plus !")
-            elif score > nb:
-                print("Moins !")
-            else:
+        while True:
+            play_one_game(min_nb, max_nb, attempts_max)
+            
+            relaunch, use_same_params = relaunch_game()
+            if not relaunch:
                 print("---------------------<3---------------------")
-                print("Bravo ! Vous avez trouvé le nombre ", "en", attempts, "essai(s)")
-            attempts += 1
+                print("Fin de la partie !")
+                print("---------------------<3---------------------")
+                break
+            elif not use_same_params:
+                break
 
-        if attempts > attempts_max and score != nb:
-            print("---------------------<3---------------------")
-            print("Vous n'avez plus aucune tentative.")
-            print("Dommage ! Le bon nombre était : ", nb, ".")
-            print("---------------------<3---------------------")
-
-        relaunch, use_same_params = relaunch_game()
         if not relaunch:
-            print("---------------------<3---------------------")
-            print("Fin de la partie !")
-            print("---------------------<3---------------------")
             break
-        elif use_same_params:
-            attempts = 1
-            score = 0
-        else:
-            break
-    if not relaunch:
-        break
+
+launch_game()
+
