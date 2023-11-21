ca8f44f25d0ea17bb8a02cc877db8c704cccdc1e
Kikii95 <147034133+Kikii95@users.noreply.github.com>
2023-11-21 14:14:48 +0100

    fin

--- plus-ou-moins.py
+++ plus-ou-moins.py
@@ -11,23 +11,32 @@ def AskInt(message: str, min_val: int, max_val: int) -> int:
         except ValueError:
             print("Veuillez entrer un nombre entier valide.")
 
+def relaunch_game():
+    reponse = input("Souhaitez-vous relancer une partie (Y/N) ? ")
+    if reponse.upper() == "Y":
+        use_same_params = input("Souhaitez-vous utiliser les mêmes paramètres (Y/N) ? ").upper() == "Y"
+        return True, use_same_params
+    return False, False
+
+print("---------------------<3---------------------")
+print("Bienvenue sur ce jeu de plus ou moins !")
+reponse = input("Souhaitez-vous consulter les règles (Y/N) ? ")
+if reponse.upper() == "Y":
+    print("---------------------<3---------------------")
+    print("Règles :")
+    print("- Le but du jeu est de deviner le chiffre choisi par l'ordinateur. - ")
+    print("------> - Définissez le nombre de tentatives que vous aurez,")
+    print("------> - Choisissez 2 bornes entre lesquelles un chiffre sera choisi aléatoirement,")
+    print("------> - Choisissez un nombre compris entre les bornes,")
+    print("------> - Plus ! alors votre nombre est plus petit que celui de l'ordinateur,")
+    print("------> - Moins ! alors votre nombre est plus grand que celui de l'ordinateur,")
+    print("------> - Attention à bien surveiller vos tentatives restantes !")
+    print("---------------------<3---------------------")
+        
 while True:
     print("---------------------<3---------------------")
-    print("Bienvenue sur ce jeu de plus ou moins !")
-    reponse = input("Souhaitez-vous consulter les règles (Y/N) ? ")
-    if reponse.upper() == "Y":
-        print("---------------------<3---------------------")
-        print("Règles :")
-        print("- Le but du jeu est de deviner le chiffre choisi par l'ordinateur. - ")
-        print("------> - Définissez le nombre de tentatives que vous aurez,")
-        print("------> - Choisissez 2 bornes entre lesquelles un chiffre sera choisi aléatoirement,")
-        print("------> - Choisissez un nombre compris entre les bornes,")
-        print("------> - Plus ! alors votre nombre est plus petit que celui de l'ordinateur,")
-        print("------> - Moins ! alors votre nombre est plus grand que celui de l'ordinateur,")
-        print("------> - Attention à bien surveiller vos tentatives restantes !")
-        print("---------------------<3---------------------")
-
     attempts_max = AskInt("Choisissez le nombre de tentatives possibles : ", 1, 100)
+    
     attempts = 1
     score = 0
 
@@ -45,42 +54,44 @@ while True:
         print("Les bornes ne sont pas valides. Veuillez recommencer.")
         continue
 
-    nb = randint(min_nb, max_nb)
+    while True:
+        nb = randint(min_nb, max_nb)
 
-    print("---------------------<3---------------------")
-    print("Les bornes sont ", min_nb, " et ", max_nb, ".")
-    print("Trouvez le nombre en moins de ", attempts_max, " tentatives si vous voulez gagner.")
-
-    # print("À des fins de test, le nombre à deviner est :", nb)
-
-    while score != nb and attempts <= attempts_max:
-        remaining_attempts = attempts_max - attempts + 1
         print("---------------------<3---------------------")
-        print("Il vous reste", remaining_attempts, "tentatives.")
-        score = AskInt("Choisissez un nombre : ", 1, 100)
+        print("Les bornes sont ", min_nb, " et ", max_nb, ".")
+        print("Trouvez le nombre en moins de ", attempts_max, " tentatives si vous voulez gagner.")
 
-        if score < nb:
-            print("Plus !")
-        elif score > nb:
-            print("Moins !")
-        else:
+        while score != nb and attempts <= attempts_max:
+            remaining_attempts = attempts_max - attempts + 1
             print("---------------------<3---------------------")
-            print("Bravo ! Vous avez trouvé le nombre ", "en", attempts, "essai(s)")
-        attempts += 1
+            print("Il vous reste", remaining_attempts, "tentatives.")
+            score = AskInt("Choisissez un nombre : ", 1, 100)
 
-    if attempts > attempts_max and score != nb:
-        print("---------------------<3---------------------")
-        print("Vous n'avez plus aucune tentative.")
-        print("Dommage ! Le bon nombre était : ", nb, ".")
-        print("---------------------<3---------------------")
+            if score < nb:
+                print("Plus !")
+            elif score > nb:
+                print("Moins !")
+            else:
+                print("---------------------<3---------------------")
+                print("Bravo ! Vous avez trouvé le nombre ", "en", attempts, "essai(s)")
+            attempts += 1
 
-    reponse = input("Souhaitez-vous relancer une partie (Y/N) ? ")
-    if reponse.upper() != "Y":
-        print("---------------------<3---------------------")
-        print("Fin de la partie !")
-        ("---------------------<3---------------------")
+        if attempts > attempts_max and score != nb:
+            print("---------------------<3---------------------")
+            print("Vous n'avez plus aucune tentative.")
+            print("Dommage ! Le bon nombre était : ", nb, ".")
+            print("---------------------<3---------------------")
+
+        relaunch, use_same_params = relaunch_game()
+        if not relaunch:
+            print("---------------------<3---------------------")
+            print("Fin de la partie !")
+            print("---------------------<3---------------------")
+            break
+        elif use_same_params:
+            attempts = 1
+            score = 0
+        else:
+            break
+    if not relaunch:
         break
-    else:
-        print("---------------------<3---------------------")
-        print("Fin de la partie !")
-        ("---------------------<3---------------------")
